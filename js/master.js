5f64ed1ff5fa1980cb8b613d7df419adc1347f15
MatthewB384 <74159499+MatthewB384@users.noreply.github.com>
2022-05-22 18:07:13 +1000

    Add files via upload

--- js/master.js
+++ js/master.js
@@ -1,50 +1,44 @@
-function tabbable(elem) {
-    return [
-        "a",
-        "area",
-        "button",
-        "input",
-        "object",
-        "select",
-        "textarea",
-    ].includes(elem.tagName.toLocaleLowerCase()) || elem.onclick !== null
-        ? true
-        : false;
+const popups = {
+    //id of popup: whether it's open or not
+    "#navmenu": false,
+};
+
+function open_popup(selector) {
+    popups[selector] = true;
+    let elem = document.querySelector(selector);
+    elem.style.display = "block"; //closed popups have display none
+    setTimeout(() => elem.classList.add("open"), 0); //has to be a settimeout otherwise the transition doesnt play
 }
-function recursive_tab_lock(elem, generation_count = 0) {
-    if (tabbable(elem)) {
-        elem.tabIndex = -1;
-    }
-    if (generation_count > 0) {
-        for (const child of elem.children) {
-            recursive_tab_lock(child, generation_count - 1);
+
+function close_popup(selector) {
+    popups[selector] = false;
+    let elem = document.querySelector(selector);
+    elem.classList.remove("open");
+    setTimeout(function () {
+        if (!popups[selector]) {
+            //if you haven't opened it again in the meantime
+            elem.style.display = "none"; //removes it from tabindex
         }
-    }
-}
-function recursive_tab_unlock(elem, generation_count = 0) {
-    if (tabbable(elem)) {
-        elem.tabIndex = 0;
-    }
-    if (generation_count > 0) {
-        for (const child of elem.children) {
-            recursive_tab_unlock(child, generation_count - 1);
-        }
-    }
+    }, 1000);
 }
 
-function open_nav_menu() {
-    menu = document.querySelector(".navmenu");
-    menu.classList.add("open");
-    recursive_tab_unlock(menu, 2);
-}
+document.onclick = function (event) {
+    for (const popup of Object.keys(popups)) {
+        if (
+            event.target.closest(popup) !== null || //you clicked on that popup
+            (event.target.closest("label") !== null && //or you clicked on a label
+                "#" + event.target.closest("label").htmlFor == popup) //and the label was for that popup
+        )
+            continue; //don't close the popup
+        close_popup(popup); //otherwise do close it
+    }
+};
 
-function close_nav_menu() {
-    menu = document.querySelector(".navmenu");
-    menu.classList.remove("open");
-    recursive_tab_lock(menu, 2);
-}
-
-recursive_tab_lock(document.querySelector(".navmenu"), 2);
-document.main.onclick = () => {
-    console.log("yo");
+document.onkeyup = function (event) {
+    switch (event.key) {
+        case "Escape": //closes all popups
+            for (const popup of Object.keys(popups)) {
+                close_popup(popup);
+            }
+    }
 };
