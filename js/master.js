95ab40a7aa4067010c1b2d0cab279e55fb69c06e
MatthewB384 <74159499+MatthewB384@users.noreply.github.com>
2022-05-24 22:56:55 +1000

    page autogenerate with js
    js now creates background, title, navbar, 3 lines, and bottom command palatte

--- js/master.js
+++ js/master.js
@@ -1,3 +1,7 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//controls popups including navmenu
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 const popups = ["#navmenu"];
 
 display_none_timeouts = {
@@ -28,7 +32,6 @@ function close_popup(selector) {
 }
 
 function toggle_navmenu_sect(elem) {
-    console.log(elem);
     panel = elem.parentNode.nextElementSibling;
     if (panel.classList.toggle("open")) {
         panel.style.maxHeight = panel.scrollHeight + "px";
@@ -42,11 +45,75 @@ function toggle_navmenu_sect(elem) {
     }
 }
 
-document.querySelectorAll(".nav-expand-btn").forEach((elem) => {
-    elem.onclick = (event) => {
-        toggle_navmenu_sect(event.target);
-    };
-});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//creates navmenu, 3 lines, and help button
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const main = document.querySelector("main");
+
+//generates navbar and bg gradient
+document.write(`
+<ul id="navmenu" class="navmenu collapsable">
+    <li><a href="">Home</a><button class="nav-btn" onclick="close_popup('#navmenu');">&#10006;</button>
+    </li>
+    <li><a href="home.html">IPM</a><button class="nav-btn nav-expand-btn">&#9660;</button></li>
+    <ul class="nav-expand-sect">
+        <li><a href="">First Sound 1</a></li>
+        <li><a href="">First Sound 2</a></li>
+        <li><a href="">First Sound 3</a></li>
+        <li><a href="">Rhyming 1</a></li>
+        <li><a href="">Rhyming 2</a></li>
+        <li><a href="">Rhyming 3</a></li>
+        <li><a href="">Sound Deletion 1</a></li>
+        <li><a href="">Sound Deletion 2</a></li>
+        <li><a href="">Sound Deletion 3</a></li>
+    </ul>
+    <li><a href="">PCA</a><button class="nav-btn nav-expand-btn">&#9660;</button></li>
+    <ul class="nav-expand-sect">
+        <li><a href="">Set 1</a></li>
+        <li><a href="">Set 2</a></li>
+        <li><a href="">Set 3</a></li>
+        <li><a href="">Set 4</a></li>
+        <li><a href="">Set 5</a></li>
+    </ul>
+    <li><a href="">Results</a><button class="nav-btn nav-expand-btn">&#9660;</button>
+    </li>
+    <ul class="nav-expand-sect">
+        <li><a href="">All Tests</a></li>
+        <li><a href="">Recent Tests</a></li>
+    </ul>
+</ul>
+<div class="bg-wrap">
+<div class="bg"> </div>
+</div>`);
+
+//creates heading and 3 lines
+const heading = document.createElement("div");
+heading.classList.add("heading");
+heading.innerHTML = `
+<label for='navmenu' class="nav-3-lines" onclick="open_popup('#navmenu');" tabindex="0">
+    <span></span>
+    <span></span>
+    <span></span>
+</label>
+<h1>Cool Word Thing</h1>`;
+main.insertBefore(heading, main.firstChild);
+
+//creates command palatte at bottom
+const cmdPalatte = document.createElement("div");
+cmdPalatte.classList.add("cmd-palatte");
+cmdPalatte.innerHTML = `
+<div class="clinician-info">
+    <button id="clinician-info-btn">i</button>
+    <label for="clinician-info-btn">Informa<wbr>tion for<br>Clini<wbr>cians</label>
+</div>
+<button class="see-results-btn">
+    <p>See my results</p>
+</button>`;
+main.appendChild(cmdPalatte);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//page interaction
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 document.onclick = function (event) {
     for (const popup of popups) {
